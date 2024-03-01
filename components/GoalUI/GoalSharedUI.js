0021fd0fabdae8289ba790f04db8e6d81777ee17
sudhanshu-misra <sudhanshu01misra@gmail.com>
2024-03-01 12:13:38 +0530

    updating

---
+++ components/GoalUI/GoalSharedUI.js
@@ -0,0 +1,140 @@
+import {View, Text,StyleSheet,ScrollView,TouchableOpacity} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {COLORS} from '../../constants/theme';
+import GoalIcons from './GoalIcons';
+// import { VictoryPie } from 'victory-native';
+// import {Svg} from 'react-native-svg';
+// import Pie from 'react-native-pie';
+// import {PieChart} from 'react-native-gifted-charts';
+ 
+
+const GoalSharedUI = ({name, icon , onClick}) => {
+  return (
+
+    
+    <View className=" mt-2 w-max">
+      
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+     
+    </View>
+    
+
+      <View className="mx-7 flex flex-row justify-between">
+        <Text className="text-xl text-black">{name}</Text>
+        <Text className="text-lg" style={{color: `${COLORS.neutral}`}}
+         onPress={()=>onClick()}
+        >
+          
+          <Icon name="plus" size={16}>
+            {' '}
+          </Icon>{' '}
+          Add new{' '}
+        </Text>
+        
+      </View>
+      
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity style={{ width: 85, height: 85, padding: 3, margin: 2.5, borderWidth: 1.5, borderColor: '#888', borderRadius: 5, justifyContent: 'center', alignItems: 'center' }}>
+          <View style={styles.container}>
+      <Icon name="shopping-bag" size={23} color="#FF5733" />
+    
+    </View>
+            <View style={{ marginTop: 1 }}>
+              <Text style={{ color: '#333', fontSize: 14, textAlign: 'center' }}>Buy item 1</Text>
+            </View>
+            <View style={{ marginTop: 1 }}>
+              <Text style={{ color: '#666', fontSize: 10, textAlign: 'center' }}>Rs 0</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{ width: 85, height: 85, padding: 3, margin: 2.5, borderWidth: 1.5, borderColor: '#888', borderRadius: 5, justifyContent: 'center', alignItems: 'center' }}>
+          <View style={styles.container}>
+      <Icon name="shopping-bag" size={23} color="orange" />
+  </View>
+            <View style={{ marginTop: 1 }}>
+              <Text style={{ color: '#333', fontSize: 14, textAlign: 'center' }}>Buy item 2</Text>
+            </View>
+            <View style={{ marginTop: 1 }}>
+              <Text style={{ color: '#666', fontSize: 10, textAlign: 'center' }}>Rs 0</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity style={{ width: 85, height: 85, padding: 3, margin: 2.5, borderWidth: 1.5, borderColor: '#888', borderRadius: 5, justifyContent: 'center', alignItems: 'center' }}>
+          <View style={styles.container}>
+      <Icon name="shopping-bag" size={23} color="#33FF57" />
+  </View>
+            <View style={{ marginTop: 1 }}>
+              <Text style={{ color: '#333', fontSize: 14, textAlign: 'center' }}>Buy item 3</Text>
+            </View>
+            <View style={{ marginTop: 1 }}>
+              <Text style={{ color: '#666', fontSize: 10, textAlign: 'center' }}>Rs 0</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{ width: 85, height: 85, padding: 3, margin: 2.5, borderWidth: 1.5, borderColor: '#888', borderRadius: 5, justifyContent: 'center', alignItems: 'center' }}>
+          <View style={styles.container}>
+      <Icon name="shopping-bag" size={25} color='#3357FF' />
+  </View>
+            <View style={{ marginTop: 1 }}>
+              <Text style={{ color: '#333', fontSize: 14, textAlign: 'center' }}>Buy item 4</Text>
+            </View>
+            <View style={{ marginTop: 1 }}>
+              <Text style={{ color: '#666', fontSize: 10, textAlign: 'center' }}>Rs 0</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
+  
+
+
+    <View className="w-max h-auto p-4 m-5 border-[2.5px] rounded-[20px] border-slate-400 flex justify-center items-center">
+        <GoalIcons icon={icon}></GoalIcons>
+        <Text className="text-lg text-center px-10 text-slate-400">
+          Your {name} details will be displayed here
+        </Text>
+        
+      </View>
+      
+    </View>
+
+    
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f9f9f9',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+  },
+  itemName: {
+    fontSize: 16,
+    color: COLORS.darkText,
+  },
+  itemValue: {
+    fontSize: 14,
+    color: COLORS.lightText,
+  },
+  // ... include any other styles from the date you have ...
+});
+
+export default GoalSharedUI;
--- screens/Goal/Goal.js
+++ screens/Goal/Goal.js
@@ -1,24 +1,29 @@
-
-import React, {useState} from 'react';
-import {View, ScrollView} from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView, StyleSheet, FlatList, Text, TouchableOpacity } from 'react-native';
 import CustomHeader from '../../components/Header';
-import DashboardSharedUI from '../../components/DashBoardUI/DashBoardSharedUI';
+import GoalSharedUI from '../../components/GoalUI/GoalSharedUI';
 import Modal from '../../components/Modal/Modal';
 import SuccessModal from '../../components/Modal/SuccessModal';
 import GoalForm from './GoalForm/GoalForm';
+import PieChart from 'react-native-pie-chart';
 
 const temp_data = [
   {
-    GoalName:"Goal"
+    GoalName: "Goal"
   }
 ];
 
-
+const data = [
+  { key: 'Buy Item 1', startDate: '01/01/2024', endDate: '12/31/2024', Amount: 6000, rsLeft: 1000, totalRsSaving: 5000 },
+  { key: 'Buy Item 2', startDate: '01/01/2024', endDate: '12/31/2024', Amount: 8000, rsLeft: 2000, totalRsSaving: 6000 },
+  { key: 'Buy Item 3', startDate: '01/01/2024', endDate: '12/31/2024', Amount: 8500, rsLeft: 1500, totalRsSaving: 7000 },
+  { key: 'Buy Item 4', startDate: '01/01/2024', endDate: '12/31/2024', Amount: 9200, rsLeft: 1200, totalRsSaving: 8000 },
+];
 
 export default function GoalHome({navigation}) {
   const [isModalVisible, SetModalVisible] = useState(false);
   const [successVisible, SetSuccessVisible] = useState(false);
-
+  const [showAllItems, setShowAllItems] = useState(false);
 
   const goalHandler = () => {
     SetModalVisible(true);
@@ -26,43 +31,173 @@ export default function GoalHome({navigation}) {
 
   const onSubmit = () => {
     SetModalVisible(false);
-    //data extraction here 
-    // if(data) received then set success visible  else show error modal with message 
-    if(temp_data){
-       SetSuccessVisible(true);
-    }
-    else{
+    if (temp_data) {
+      SetSuccessVisible(true);
+    } else {
       //error modal
     }
+  };
 
-   
+  const widthAndHeight = 170;
+  const series = [100, 100, 100, 100];
+  const sliceColor = [
+    'rgba(255, 87, 51, 0.5)',
+    'rgba(255, 165, 0, 0.5)',
+    'rgba(51, 255, 87, 0.5)',
+    'rgba(51, 87, 255, 0.5)',
+  ];
+
+  const totalAmount = data.reduce((acc, item) => acc + item.Amount, 0);
+
+  
+  const renderItem = ({ item, index }) => {
+    if (!showAllItems && index > 1) {
+      return null; // Hide items 3 and 4 if showAllItems is false
+    }
+
+    const totalAmount = item.rsLeft + item.totalRsSaving;
+
+    return (
+      <View style={[styles.item, { backgroundColor: getColor(index) }]}>
+        <View style={styles.itemLeft}>
+          <Text style={styles.itemTitle}>{item.key}</Text>
+          <Text>Start Date: {item.startDate}</Text>
+          <Text>End Date: {item.endDate}</Text>
+        </View>
+        <View style={styles.itemRight}>
+          <Text style={styles.amountText}> Amount: {totalAmount}</Text>
+          <Text>Rupees Left: {item.rsLeft}</Text>
+          <Text>Total Rupees Saving: {item.totalRsSaving}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  const getColor = (index) => {
+    const colors = [
+      'rgba(255, 87, 51, 1)',
+      'rgba(255, 165, 0, 1)',
+      'rgba(51, 255, 87, 1)',
+      'rgba(51, 122, 222, 1)',
+    ];
+    return colors[index % colors.length];
+  };
+
+  const toggleItems = () => {
+    setShowAllItems(!showAllItems);
   };
 
   return (
-      <View>
-        <CustomHeader navigation={navigation} />
-        <ScrollView>
-          <DashboardSharedUI
-            name="Goals"
-            icon="piggy-bank"
-            onClick={goalHandler}></DashboardSharedUI>
-        </ScrollView>
-      
-        <Modal modalState={isModalVisible}  hideModal={() => SetModalVisible(false)}>
-          <GoalForm
-            hideModal={() => SetModalVisible(false)}
-            onSubmit={onSubmit}></GoalForm>
-        </Modal>
-
-        {temp_data && (
-          <SuccessModal
-            modalState={successVisible}
-            hideModal={() => SetSuccessVisible(false)}
-            formData={temp_data[0]}
-            ></SuccessModal>
-        )}
-      </View>
-
+    <View style={styles.container}>
+      <CustomHeader navigation={navigation} />
+      <ScrollView>
+        <View style={styles.chartContainer}>
+          <PieChart
+            widthAndHeight={widthAndHeight}
+            series={series}
+            sliceColor={sliceColor}
+            coverRadius={0.45}
+            coverFill={'#FFF'}
+            doughnut={true}
+          />
+          <View style={styles.centerTextContainer}>
+            <Text style={styles.centerText}>Goal</Text>
+            <Text style={styles.totalAmountText}>Rs {totalAmount}</Text>
+          </View>
+        </View>
+        <View style={styles.flatListContainer}>
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={item => item.key}
+            style={styles.flatList}
+          />
+          {data.length > 2 && (
+            <TouchableOpacity onPress={toggleItems} style={styles.viewAllButton}>
+              <Text style={styles.viewAllText}>{showAllItems ? 'View Less' : 'View All'}</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+        <GoalSharedUI name="Goal" icon="track-changes" onClick={goalHandler} />
+      </ScrollView>
+      <Modal modalState={isModalVisible} hideModal={() => SetModalVisible(false)}>
+        <GoalForm hideModal={() => SetModalVisible(false)} onSubmit={onSubmit} />
+      </Modal>
+      {temp_data && (
+        <SuccessModal
+          modalState={successVisible}
+          hideModal={() => SetSuccessVisible(false)}
+          formData={temp_data[0]}
+        />
+      )}
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  chartContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  centerTextContainer: {
+    position: 'absolute',
+    alignItems: 'center',
+  },
+  centerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 62,
+  },
+  totalAmountText: {
+    fontSize: 14,
+    marginBottom: 10,
+  },
+  flatListContainer: {
+    marginTop: 10,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+  },
+  flatList: {
+    flexGrow: 1,
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 8,
+  },
+  itemLeft: {
+    flex: 1,
+  },
+  itemRight: {
+    alignItems: 'flex-end',
+  },
+  itemTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  amountText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 32,
+  },
+  viewAllButton: {
+    alignItems: 'center',
+    paddingVertical: 8,
+  },
+  viewAllText: {
+    color: 'blue',
+    fontWeight: 'bold',
+  },
+});
